4856339278b91d074ac749d89cddc54215e52f25
lnxcz <david.frnoch@creativehill.cz>
2022-11-01 14:08:15 +0100

    feat: :sparkles: add viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>lynx</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Fullstack developer" />
         <link rel="icon" href="/favicon.ico" />
       </head>
